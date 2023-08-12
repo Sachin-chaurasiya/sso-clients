78ef6d3e63f39ea5f0e96958e1c385df39f1abdb
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-08-12 23:05:20 +0530

    chore: track test coverage (#6)
    * chore: track test coverage
    
    * chore: add json summary reporter
    
    * Update unit-test.yml

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '__tests__/.*\\.test\\.ts$',
+  collectCoverage: true,
+  coverageReporters: ['text-summary', 'json-summary'],
 };
