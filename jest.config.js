452f3ebe0df572cd8d8eef8776811a4e990aa3e1
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-08-12 22:48:01 +0530

    chore: track test coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '__tests__/.*\\.test\\.ts$',
+  collectCoverage: true,
+  coverageReporters: ['json', 'text-summary'],
 };
